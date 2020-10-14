85acb2d5800f7b53be3a7b7c66d14a7d3fbb8608
Kym Hooper <khoupp@gmail.com>
2020-10-14 10:32:08 -0600

    test

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = 3001;
 app.use(express.static('../client/dist'));
 
 // this is all fine
-
+// another comment
 app.listen(PORT, () => {
   console.log(`Server running and listening now on port: ${PORT}`);
 });
