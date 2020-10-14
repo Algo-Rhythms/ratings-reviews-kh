ef23670b2ff33ebd26ed1cdc74724bf4bca9744f
Kym Hooper <khoupp@gmail.com>
2020-10-14 11:41:24 -0600

    get/post/put

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ const PORT = 3001;
 app.use(express.static('../client/dist'));
 
 // GET
-// app.get('/reviews/:product_id/list', (req, res) => {
-//   name((err, results) => {
-//     if (err) {
-//       res.sendStatus(404, error);
-//     } else {
-//       res.send(results);
-//     }
-//   })
-// })
+app.get('/reviews/:product_id/list', (req, res) => {
+  name((err, results) => {
+    if (err) {
+      res.sendStatus(404, error);
+    } else {
+      res.send(results);
+    }
+  })
+})
 // Status: 200 OK
 // Parameter	Type	Description
 // product_id	integer	Specifies the product for which to retrieve reviews.
@@ -24,54 +24,54 @@ app.use(express.static('../client/dist'));
 // sort	text	Changes the sort order of reviews to be based on "newest", "helpful", or "relevant"
 
 // GET
-// app.get('/reviews/:product_id/meta', (req, res) => {
-//   name((err, results) => {
-//     if (err) {
-//       res.sendStatus(404, error);
-//     } else {
-//       res.send(results);
-//     }
-//   })
-// })
+app.get('/reviews/:product_id/meta', (req, res) => {
+  name((err, results) => {
+    if (err) {
+      res.sendStatus(404, error);
+    } else {
+      res.send(results);
+    }
+  })
+})
 // Status: 200 OK
 // product_id	integer	Required ID of the product for which data should be returned
 
 // POST
-// app.post('/reviews/:product_id', (req, res) => {
-//   let var = req.body.;
-//   name(var, (err, results) => {
-//     if (err) {
-//       res.sendStatus(404, err);
-//     } else {
-//       res.sendStatus(201);
-//     }
-//   })
-// })
+app.post('/reviews/:product_id', (req, res) => {
+  let var = req.body.;
+  name(var, (err, results) => {
+    if (err) {
+      res.sendStatus(404, err);
+    } else {
+      res.sendStatus(201);
+    }
+  })
+})
 // Status: 201 CREATED
 
 // PUT
-// app.put('/reviews/helpful/:review_id', (req, res) => {
-//   let var = req.params.;
-//   name(var, var, (err, results) => {
-//     if (err) {
-//       res.sendStatus(204, err);
-//     } else {
-//       res.sendStatus(201);
-//     }
-//   })
-// });
+app.put('/reviews/helpful/:review_id', (req, res) => {
+  let var = req.params.;
+  name(var, var, (err, results) => {
+    if (err) {
+      res.sendStatus(204, err);
+    } else {
+      res.sendStatus(201);
+    }
+  })
+});
 
 // PUT
-// app.put('/reviews/report/:review_id', (req, res) => {
-//   let var = req.params.;
-//   name(var, var, (err, results) => {
-//     if (err) {
-//       res.sendStatus(204, err);
-//     } else {
-//       res.sendStatus(201);
-//     }
-//   })
-// });
+app.put('/reviews/report/:review_id', (req, res) => {
+  let var = req.params.;
+  name(var, var, (err, results) => {
+    if (err) {
+      res.sendStatus(204, err);
+    } else {
+      res.sendStatus(201);
+    }
+  })
+});
 
 // whats up
 
