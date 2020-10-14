c46e5f384a55233527a1679c9dfb18563bef6ea2
Kym Hooper <khoupp@gmail.com>
2020-10-14 11:53:14 -0600

    Prep database index, server index, and queries

---
+++ database/index.js
@@ -0,0 +1,11 @@
+// const postgres = require("mysql");
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "",
+// });
+
+// connection.connect();
+
+// module.exports = connection;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 // const {} = require("");
 
