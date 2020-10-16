1f3610f261d349dca6726bca67f87cb958e6ec85
Kym Hooper <khoupp@gmail.com>
2020-10-15 19:24:57 -0600

    Skeletonized queries, schema, index files

---
+++ database/mongoschema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const productSch = new Schema {
+  // fields
+  // how to handle the relationality?
+  //  // match ids to ids
+}
+
+const reviewSch = new Schema {}
+
+const characteristicsSch = new Schema {}
+
+const photoSch = new Schema {}
+
+// compass community : mongoose <<>> pgadmin : postgres //
