16092b3cf076657b759d520fb4e2f7344216d5bf
Kym Hooper <khoupp@gmail.com>
2020-10-16 20:45:23 -0600

    Schema file, dbconnect file, queries, index.js

--- database/queries.js
+++ database/queries.js
@@ -1,1 +1,17 @@
-// const dbconnection = require('../database/index.js');
+const dbconnection = require('../database/dbconnect.js');
+
+const addNewReview = (messageObj, productId) => {
+    let {rating, summary, body, recommend, name, email, photos, characteristics} = messageObj;
+    dbconnection.query(`make sql here; send to db to insert into table reviews where etc ...`)
+}
+
+module.exports = {addNewReview, }
+
+// dbconnection.query("DELETE FROM products WHERE name = $1", ['John Doe'], (err, res) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(res);
+//     }
+//     dbconnection.end();
+// })
--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
-// const {} = require("");
+// const query = require('../database/queries');
+const {addNewReview} = require('../database/queries');
 
 app.use(express.static('../client/dist'));
+app.use(express.json());
 
 // GET
 app.get('/reviews/:product_id/list', (req, res) => {
-  name((err, results) => {
+  // req.params.product_id  === whatever num, eg 5
+  // const id = req.params.product_id;
+  (err, results) => {
     if (err) {
       res.sendStatus(404, error);
     } else {
       res.send(results);
     }
-  })
+  }
 })
 // Status: 200 OK
 // Parameter	Type	Description
@@ -27,33 +28,35 @@ app.get('/reviews/:product_id/list', (req, res) => {
 
 // GET
 app.get('/reviews/:product_id/meta', (req, res) => {
-  name((err, results) => {
+  // const id = req.params.product_id;
+  (err, results) => {
     if (err) {
       res.sendStatus(404, error);
     } else {
       res.send(results);
     }
-  })
+  }
 })
 // Status: 200 OK
 // product_id	integer	Required ID of the product for which data should be returned
 
 // POST
 app.post('/reviews/:product_id', (req, res) => {
-  let var = req.body.;
-  name(var, (err, results) => {
+  // let {rating, summary, body, recommend, name, email, photos, characteristics} = req.body;
+  // const id = req.params.product_id;
+  (err, results) => {
     if (err) {
       res.sendStatus(404, err);
     } else {
       res.sendStatus(201);
     }
-  })
+  }
 })
 // Status: 201 CREATED
 
 // PUT
 app.put('/reviews/helpful/:review_id', (req, res) => {
-  let var = req.params.;
+  // const helpful = req.params.review_id;
   name(var, var, (err, results) => {
     if (err) {
       res.sendStatus(204, err);
@@ -65,7 +68,7 @@ app.put('/reviews/helpful/:review_id', (req, res) => {
 
 // PUT
 app.put('/reviews/report/:review_id', (req, res) => {
-  let var = req.params.;
+  // const helpful = req.params.review_id;
   name(var, var, (err, results) => {
     if (err) {
       res.sendStatus(204, err);
@@ -74,9 +77,6 @@ app.put('/reviews/report/:review_id', (req, res) => {
     }
   })
 });
-
-// whats up
-
 
 app.listen(PORT, () => {
   console.log(`Server running and listening now on port: ${PORT}`);
